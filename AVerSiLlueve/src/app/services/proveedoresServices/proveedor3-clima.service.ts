2fd31411a63b2bffbe4e94d3e268389cd59e1ac2
arthurcp96 <arthurcp96@gmail.com>
2024-10-15 17:47:23 -0300

    Union rama Arthur y Lucas. Correccion de implementacion de servicio proveedor2 y proveedor3

--- AVerSiLlueve/src/app/services/proveedoresServices/proveedor3-clima.service.ts
+++ AVerSiLlueve/src/app/services/proveedoresServices/proveedor3-clima.service.ts
@@ -8,8 +8,7 @@ export class Proveedor3ClimaService {
 
   apiKey = 'c760af7a74a4ec3543b1ddd7f09e9636'; //ES CONSTANTE, POR ESO LA DECLARO ACA
   URI : string = '';
-  lat : string = ''; //? lo establecemos como vacio para que despues lo rellene el usuario a traves de la funcion obtenerDatos1
-  lon : string = ''; //? lo establecemos como vacio para que despues lo rellene el usuario a traves de la funcion obtenerDatos1
+
 
   constructor(
     public http: HttpClient
@@ -18,13 +17,13 @@ export class Proveedor3ClimaService {
     //Yo lo habia pensado como:
     //this.URI=`https://api.openweathermap.org/data/2.5/forecast?lat={${this.lat}}&lon={${this.lon}}&appid=${this.apiKey}`;
     //en el video lo muestra como:
-    this.URI=`https://api.openweathermap.org/data/2.5/weather?&appid=${this.apiKey}&units=metric&q=`;
+    //this.URI=`https://api.openweathermap.org/data/2.5/weather?&appid=${this.apiKey}&units=metric&q=`;
   }
 
   //CREAMOS METODO PARA OBTENER DATOS API
   obtenerDatos3(lat: string, lon: string)
   {
-    return this.http.get(`${this.URI}${lat},${lon}`);
+    return this.http.get(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${this.apiKey}`);
   }
 
 }
