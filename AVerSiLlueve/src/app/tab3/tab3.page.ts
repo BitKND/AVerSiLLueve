92ff630077be9bdc38d5706659890511b8f97254
Lucas <lucasfernandez231@gmail.com>
2024-10-18 15:40:39 -0300

    front del tab2(falta terminar algunas cosas de devolucion de info)

--- AVerSiLlueve/src/app/tab3/tab3.page.ts
+++ AVerSiLlueve/src/app/tab3/tab3.page.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from '../services/authServices/authentication.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tab3',
@@ -6,7 +8,25 @@ import { Component } from '@angular/core';
   styleUrls: ['tab3.page.scss']
 })
 export class Tab3Page {
+  user:any;
 
-  constructor() {}
+  constructor(
+    public authService:AuthenticationService,
+    public route: Router,
+/* 
+    public proveedorClimaService: ProveedorClimaService,
+    public proveedor2ClimaService: Proveedor2ClimaService,
+    public proveedor3ClimaService: Proveedor3ClimaService */
 
+  ) {
+    this.user = authService.getProfile();
+  }
+
+  async logout(){
+    this.authService.signOut().then(()=>{
+      this.route.navigate(['/sign-in'])
+    }).catch((error)=>{
+      console.log(error);
+    })
+  }
 }
