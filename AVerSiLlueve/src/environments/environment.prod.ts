88c4dd7fea02dee1416c247da5b2b6dade490364
cristian saga <cristianrojo93@yahoo.com.ar>
2025-06-25 20:54:33 -0300

    modificado environment.prod

--- AVerSiLlueve/src/environments/environment.prod.ts
+++ AVerSiLlueve/src/environments/environment.prod.ts
@@ -7,5 +7,7 @@ export const environment = {
     storageBucket: "aversillueve-8e165.appspot.com",
     messagingSenderId: "677722327734",
     appId: "1:677722327734:web:47159b7885c6e9a2cb3417"
-  }
+  },
+  lambdaApiUrl: 'https://kn2cz0a0k8.execute-api.us-east-1.amazonaws.com/dev/usuarios',
+  openWeatherApiKey: 'c760af7a74a4ec3543b1ddd7f09e9636'
 };
